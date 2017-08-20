83ef64d6057ce6a2e9e8dd1b0fb51a3280a67377
chajr <chajr@bluetree.pl>
2017-08-20 16:29:42 +0200

    Added empty test for File storage test

---
+++ tests/Storage/FileTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace SimpleLog\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class FileTest extends TestCase
+{
+    /**
+     * actions launched before test starts
+     */
+    protected function setUp()
+    {
+        
+    }
+
+    /**
+     * actions launched after test was finished
+     */
+    protected function tearDown()
+    {
+        
+    }
+}
