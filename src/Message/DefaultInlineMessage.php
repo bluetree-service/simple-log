fb5f318f655e5541251443c1851765a8c6790e8a
chajr <chajr@bluetree.pl>
2018-01-14 11:30:50 +0100

    Added logic for default inline message

---
+++ src/Message/DefaultInlineMessage.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SimpleLog\Message;
+
+class DefaultInlineMessage extends DefaultMessage
+{
+    /**
+     * @return $this
+     */
+    protected function wrapMessage()
+    {
+        $date = strftime('%d-%m-%Y - %H:%M:%S', time());
+        $this->message = '[' . $date . '] ' . $this->message;
+
+        return $this;
+    }
+
+    /**
+     * @param string|int $key
+     * @param mixed $value
+     * @return $this
+     */
+    protected function processMessage($key, $value)
+    {
+        $row = ' | ';
+
+        if (!is_int($key)) {
+            $row .= $key . ':';
+        }
+
+        if (is_array($value)) {
+            $this->message .= $row;
+            $this->buildMessage(' | ' . $value);
+        } else {
+            $this->message .= $row . $value;
+        }
+
+        return $this;
+    }
+}
